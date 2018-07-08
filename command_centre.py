3ab2b3b871635e66e4d82c3f97ad5ad23464e4d6
Sam <js.henderson@live.co.uk>
2018-07-08 18:05:03 +0100

    Forgot to commit. Still window image problem.

--- command_centre.py
+++ command_centre.py
@@ -2,7 +2,7 @@ import sys
 import signal
 from PyQt5.QtWidgets import QApplication, QWidget, QDesktopWidget
 from PyQt5.QtWidgets import QLabel, QGridLayout, QSizePolicy
-from PyQt5.QtWidgets import QGraphicsOpacityEffect, QLabel
+from PyQt5.QtWidgets import QGraphicsOpacityEffect, QLabel, QPushButton
 from PyQt5.QtGui import QIcon, QColor, QPalette, QIcon, QFont
 from PyQt5.QtSvg import QSvgWidget
 from PyQt5.QtCore import pyqtSignal, QObject
@@ -19,7 +19,7 @@ class monitor_thread(QThread):
 
     def run(self):
 
-        self.state = self.launcher.dev
+        self.state = self.launcher.check_connection()
 
         while True:
             self.new_state = self.launcher.check_connection()
@@ -38,7 +38,6 @@ class boot_window_monitor(QThread):
             super().__init__()
             self.launcher = launcher
 
-
         def run(self):
             running = True
             while running:
@@ -52,110 +51,6 @@ class boot_window_monitor(QThread):
                         self.sleep(0.1)
                 else:
                     self.sleep(0.1)
-
-class command_centre(QWidget):
-
-    def __init__(self):
-        super().__init__()
-        self.launcher = tenx()
-        self.launcher.connect_launcher()
-        self.boot_screen = boot_screen()
-        self.command_screen = command_screen()
-        self.screen = "boot"
-        self.boot_screen.show()
-        self.spawn_monitors()
-        self.connection_monitor.start()
-        self.boot_window_monitor.start()
-
-    def usb_toggle(self):
-        self.launcher.connect_launcher()
-        self.launcher.tripped = True
-        self.update_usb_image()
-        self.update_boot_label()
-        self.change_window()        
-
-    def change_window(self):
-        if (self.screen == "control")
-            self.screen = "boot"
-            print("Moved to boot screen")
-            self.command_screen.hide()
-            self.boot_screen.show()
-            self.boot_window_monitor.start()
-        else:
-            self.screen = "control"
-            print("Moved to control screen")
-            self.boot_screen.hide()
-            self.command_screen.show()
-
-    def spawnn_monitors(self):
-        self.connection_monitor = monitor_thread(self.launcher)
-        self.connection_monitor.signal.connect(self.usb_toggle)
-        self.boot_window_monitor = boot_window_monitor(self.launcher)
-        self.boot_window_monitor.signal.connect(self.change_window)
-
-
-class boot_screen(my_window):
-
-    def __init__(self, launcher):
-        super().__init__()
-        self.launcher = launcher
-        icon_str = "./Images/missile.png"
-        window_str = "Missile Command"
-        self.setWindowTitle(window_str)
-        self.add_icon(icon_str)
-        self.determine_geometry()
-        self.initUI(2, 2)
-
-    def add_widgets(self):
-        self.grid()
-        self.add_usb_image()
-        self.add_boot_label()
-
-    def add_boot_label(self):
-        self.boot_label = QLabel()
-        font = QFont('Droid Sans', 16)
-        font.setBold(True)
-        self.boot_label.setFont(font)
-        text_color = QColor()
-        text_color.setNamedColor(self.grey)
-        pal = QPalette()
-        pal.setColor(QPalette.WindowText, text_color)
-        self.boot_label.setPalette(pal)
-        self.grid.addWidget(self.boot_label, 0, 0, Qt.AlignCenter)
-        self.spacing_label = QLabel()
-        self.grid.addWidget(self.spacing_label, 2, 0, Qt.AlignCenter)
-        self.update_boot_label()
-
-    def add_usb_image(self):
-
-        self.usb_image = QSvgWidget()
-        self.usb_image.image0 = './Images/usb-0.svg'
-        self.usb_image.image1 = './Images/usb-1.svg'
-        self.usb_image.max_height = self.height/2
-        self.grid.addWidget(self.usb_image, 1, 0, Qt.AlignHCenter)
-        self.update_usb_image()
-
-    def update_usb_image(self):
-
-        if (self.launcher.dev is None):
-            self.usb_image.load(self.usb_image.image0)
-            self.usb_image.aspect_ratio = 1.0
-        else:
-            self.usb_image.load(self.usb_image.image1)
-            self.usb_image.aspect_ratio = 1.0
-
-        height = self.usb_image.max_height
-        width = height*self.usb_image.aspect_ratio
-        self.usb_image.setFixedWidth(width)
-        self.usb_image.setFixedHeight(height)
-        self.animate_svg(self.usb_image)
-
-    def update_boot_label(self):
-        if (self.launcher.dev is None):
-            self.boot_label.setText("Please insert your Tenx launcher")
-        else:
-            self.boot_label.setText("Welcome Commander")
-
 
 class my_window(QWidget):
 
@@ -200,7 +95,7 @@ class my_window(QWidget):
         y = centre.y()
         self.move(x-self.width/2, y-self.height/2)
 
-    def grid(self):
+    def create_grid(self):
         self.grid = QGridLayout()
         self.setLayout(self.grid)
 
@@ -214,8 +109,134 @@ class my_window(QWidget):
         svg_image.animation.setEasingCurve(QEasingCurve.OutCirc)
         svg_image.animation.start()
 
+class command_centre(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.launcher = tenx()
+        self.launcher.connect_launcher()
+        self.boot_screen = boot_screen(self.launcher)
+        self.command_screen = command_screen(self.launcher)
+        self.screen = "boot"
+        self.boot_screen.show()
+        self.spawn_monitors()
+        self.connection_monitor.start()
+        self.boot_window_monitor.start()
+
+    def usb_toggle(self):
+        self.launcher.connect_launcher()
+        self.launcher.tripped = True
+        self.boot_screen.update_usb_image()
+        self.boot_screen.update_boot_label()
+
+    def change_window(self):
+        if (self.screen == "control"):
+            self.screen = "boot"
+            print("Moved to boot screen")
+            self.command_screen.hide()
+            self.boot_screen.show()
+            self.boot_window_monitor.start()
+        else:
+            self.screen = "control"
+            print("Moved to control screen")
+            self.boot_screen.hide()
+            self.command_screen.show()
+
+    def spawn_monitors(self):
+        self.connection_monitor = monitor_thread(self.launcher)
+        self.connection_monitor.signal.connect(self.usb_toggle)
+        self.boot_window_monitor = boot_window_monitor(self.launcher)
+        self.boot_window_monitor.signal.connect(self.change_window)
+
+
+class boot_screen(my_window):
+
+    def __init__(self, launcher):
+        super().__init__()
+        self.launcher = launcher
+        icon_str = "./Images/missile.png"
+        window_str = "Missile Command"
+        self.setWindowTitle(window_str)
+        self.add_icon(icon_str)
+        self.determine_geometry()
+        self.initUI(2, 2)
+        self.add_widgets()
+
     def add_widgets(self):
-        print("Please subclass and reimplement this function")
+        self.create_grid()
+        self.add_usb_image()
+        self.add_boot_label()
+
+    def add_boot_label(self):
+        self.boot_label = QLabel()
+        font = QFont('Droid Sans', 16)
+        font.setBold(True)
+        self.boot_label.setFont(font)
+        text_color = QColor()
+        text_color.setNamedColor(self.grey)
+        pal = QPalette()
+        pal.setColor(QPalette.WindowText, text_color)
+        self.boot_label.setPalette(pal)
+        self.grid.addWidget(self.boot_label, 0, 0, Qt.AlignCenter)
+        self.spacing_label = QLabel()
+        self.grid.addWidget(self.spacing_label, 2, 0, Qt.AlignCenter)
+        self.update_boot_label()
+
+    def add_usb_image(self):
+
+        self.usb_image = QSvgWidget()
+        self.usb_image.image0 = './Images/usb-0.svg'
+        self.usb_image.image1 = './Images/usb-1.svg'
+        self.usb_image.max_height = self.height/2
+        self.grid.addWidget(self.usb_image, 1, 0, Qt.AlignHCenter)
+        self.update_usb_image()
+
+    def update_usb_image(self):
+
+        print("Image Updating!")
+        if (self.launcher.dev is None):
+            self.usb_image.load(self.usb_image.image0)
+            self.usb_image.aspect_ratio = 1.0
+        else:
+            self.usb_image.load(self.usb_image.image1)
+            self.usb_image.aspect_ratio = 1.0
+
+        height = self.usb_image.max_height
+        width = height*self.usb_image.aspect_ratio
+        self.usb_image.setFixedWidth(width)
+        self.usb_image.setFixedHeight(height)
+        self.animate_svg(self.usb_image)
+
+    def update_boot_label(self):
+        print("Updating Text!")
+        if (self.launcher.dev is None):
+            self.boot_label.setText("Please insert your Tenx launcher")
+        else:
+            self.boot_label.setText("Welcome Commander")
+
+
+
+class command_screen(my_window):
+
+        def __init__(self, launcher):
+            super().__init__()
+            self.launcher = launcher
+            icon_str = "./Images/missile.png"
+            window_str = "Missile Command"
+            self.setWindowTitle(window_str)
+            self.add_icon(icon_str)
+            self.determine_geometry()
+            self.initUI(1.5, 1.5)
+
+
+        def add_widgets(self):
+            self.create_grid()
+            self.fire_button = QPushButton()
+            self.fire_button.setText("Fire")
+            # self.fire_button.clicked.connect(
+            #     self.launcher.send_cmd(self.launcher.fire))
+            self.grid.addWidget(self.fire_button, 1, 1)
+
 
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, signal.SIG_DFL)
